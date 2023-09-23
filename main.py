55543098e712f6447d343ee74cd3ddf388a9cf7f
Catea <ecaterinah@mail.ru>
2023-09-23 14:32:08 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+import pytesseract
+from PIL import Image
+img=Image.open(r"C:\Users\Asus\PycharmProjects\numbersFromPicture\img.png")
+pytesseract.pytesseract.tesseract_cmd=r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+text=pytesseract.image_to_string(img)
+print(text)
